c1f83b65fab007a5b01d35a39da0078c51f5011b
Ilya Shandulskiy <ilyashandulskiy@gmail.com>
2022-09-30 21:13:36 +0500

    Fix: connection error coverage

--- src/api/methods.js
+++ src/api/methods.js
@@ -1,24 +1,47 @@
 import axios from 'axios';
+import toast from 'react-hot-toast';
 
 const instance = axios.create({
   baseURL: 'http://localhost:8080/',
 });
 
+const onError = () => toast.error('Connection error');
+
 export default {
   async get(url) {
-    const { data } = await instance.get(url);
-    return data;
+    try {
+      const { data } = await instance.get(url);
+      return data;
+    } catch (err) {
+      onError(err);
+      return Promise.reject();
+    }
   },
   async post(url, body) {
-    const { data } = await instance.post(url, body);
-    return data;
+    try {
+      const { data } = await instance.post(url, body);
+      return data;
+    } catch (err) {
+      onError(err);
+      return Promise.reject();
+    }
   },
   async put(url, body) {
-    const { data } = await instance.put(url, body);
-    return data;
+    try {
+      const { data } = await instance.put(url, body);
+      return data;
+    } catch (err) {
+      onError(err);
+      return Promise.reject();
+    }
   },
   async drop(url) {
-    const { data } = await instance.delete(url);
-    return data;
+    try {
+      const { data } = await instance.delete(url);
+      return data;
+    } catch (err) {
+      onError(err);
+      return Promise.reject();
+    }
   },
 };
